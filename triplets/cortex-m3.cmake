41426b713926a7906480cbf56da73884e76c1dce
xlongfeng <xlongfeng@126.com>
2023-07-21 22:59:20 +0800

    Remove vcpkg
    Signed-off-by: xlongfeng <xlongfeng@126.com>

--- triplets/cortex-m3.cmake
+++
@@ -1,7 +0,0 @@
-set(VCPKG_TARGET_ARCHITECTURE cortex-m3)
-set(VCPKG_CRT_LINKAGE static)
-set(VCPKG_LIBRARY_LINKAGE static)
-
-set(VCPKG_CMAKE_SYSTEM_NAME Empty)
-
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/toolchains/arm-none-eabi.cmake)
